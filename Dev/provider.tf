aa94b2f8da7f9cc9e7ee4bb72850c14a0b02a7e6
Jitender Kumar <thejitenderk@gmail.com>
2025-04-26 11:30:26 -0400

    env secret added in yaml

--- Dev/provider.tf
+++ Dev/provider.tf
@@ -9,10 +9,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "2178b722-5baa-4418-8bc9-3fcb9cd14fed"
-  client_id       = env("ARM_CLIENT_ID")
-  client_secret   = env("ARM_CLIENT_SECRET")
-  tenant_id       = env("ARM_TENANT_ID")
-
-
 }
