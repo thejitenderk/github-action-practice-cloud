568e3b9f60c780aed9caa844d4ec81f84e8638ba
Jitender Kumar <thejitenderk@gmail.com>
2025-04-26 10:58:56 -0400

    fmt done manual

--- Dev/main.tf
+++ Dev/main.tf
@@ -1,15 +1,15 @@
 module "rg" {
-    source = "../Module/resource_group"
-    rg_name = var.rg_name
-    rg_location = var.rg_location
-  
+  source      = "../Module/resource_group"
+  rg_name     = var.rg_name
+  rg_location = var.rg_location
+
 }
 
 module "vnet" {
-    depends_on = [ module.rg ]
-    source = "../Module/virtual_network"
-    rg_name = var.rg_name
-    rg_location = var.rg_location
-    vnet_name = var.vnet_name
+  depends_on  = [module.rg]
+  source      = "../Module/virtual_network"
+  rg_name     = var.rg_name
+  rg_location = var.rg_location
+  vnet_name   = var.vnet_name
 
 }
--- Dev/variable.tf
+++ Dev/variable.tf
@@ -1,11 +1,11 @@
 variable "rg_name" {
-  
+
 }
 
 variable "rg_location" {
-  
+
 }
 
 variable "vnet_name" {
-  
+
 }
