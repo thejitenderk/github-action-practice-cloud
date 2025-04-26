fed455c922698e289625a294ac002d115db52dfb
Jitender Kumar <thejitenderk@gmail.com>
2025-04-26 11:10:04 -0400

    added secret

--- Dev/provider.tf
+++ Dev/provider.tf
@@ -10,5 +10,9 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "2178b722-5baa-4418-8bc9-3fcb9cd14fed"
+  client_id       = "52cf46d8-ada0-493f-bb01-ceee17e98566"
+  client_secret   = "ojJ8Q~o.o34uGy2~D7d.cMikSiOqghsL4rRcEdcA"
+  tenant_id       = "d1f71056-2ff0-4ce8-b1c3-b8a471e4a179"
+  
 
 }
